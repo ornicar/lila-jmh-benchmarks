2a147c64824f3e8c7487dedb3151ca1f7de3d9bd
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-05-28 23:21:22 +0200

    add escaping benchmarks

--- src/main/scala/HtmlEscape.scala
+++ src/main/scala/HtmlEscape.scala
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 class HtmlEscape {
 
-/* Results (i7 6700HQ)
+  /* Results (i7 6700HQ)
 [info] Benchmark                    Mode  Cnt     Score     Error  Units
 [info] HtmlEscape.testLongAscii     avgt   10  3504.387 ± 198.163  ns/op
 [info] HtmlEscape.testLongUnicode   avgt   10  3924.027 ± 102.305  ns/op
@@ -17,29 +17,42 @@ class HtmlEscape {
 [info] HtmlEscape.testShortUnicode  avgt   10   429.636 ±  12.383  ns/op
 */
 
-    def escape(s: String): String = {
-      val sb = new StringBuilder
-      var i = 0
-      while (i < s.length) {
-        sb.append {
-          s.charAt(i) match {
-            case '<' => "&lt;"
-            case '>' => "&gt;"
-            case '&' => "&amp;"
-            case '"' => "&quot;"
-            case '\'' => "&#39;"
-            case c => c
-          }
+  def escape(s: String): String = {
+    val sb = new StringBuilder
+    var i = 0
+    while (i < s.length) {
+      sb.append {
+        s.charAt(i) match {
+          case '<' => "&lt;"
+          case '>' => "&gt;"
+          case '&' => "&amp;"
+          case '"' => "&quot;"
+          case '\'' => "&#39;"
+          case c => c
         }
-        i += 1
       }
-      sb.toString
+      i += 1
     }
+    sb.toString
+  }
 
-  val shortAscii   = "Outside the board"
+  def escapeOptimist(s: String): String = {
+    var i = 0
+    while (i < s.length) {
+      val c = s.charAt(i)
+      if (c == '<' || c == '>' || c == '&' || c == '"' || c == '\'') return escape(s)
+      i += 1
+    }
+    s
+  }
+
+  val badChars = "[<>&\"']".r.pattern
+  def escapeOptimistRegex(s: String): String = if (badChars.matcher(s).find) escape(s) else s
+
+  val shortAscii = "Outside the board"
   val shortUnicode = "Ожидание соперниа"
 
-  val longAscii   = "Free online Chess server. Play Chess now in a clean interface. No registration, no ads, no plugin required. Play Chess with the computer, friends or random opponents."
+  val longAscii = "Free online Chess server. Play Chess now in a clean interface. No registration, no ads, no plugin required. Play Chess with the computer, friends or random opponents."
   val longUnicode = "Бесплатные онлайн-шахматы. Играйте в шахматы прямо сейчас в простом интерфейсе без рекламы. Не требует регистрации и скачивания программы. Играйте в шахматы с компьи."
 
   @Benchmark
@@ -53,5 +66,11 @@ class HtmlEscape {
 
   @Benchmark
   def testLongUnicode = escape(longUnicode)
+
+  @Benchmark
+  def testLongUnicodeOptimist = escapeOptimist(shortUnicode)
+
+  @Benchmark
+  def testLongUnicodeOptimistRegex = escapeOptimistRegex(shortUnicode)
 }
 
