517b47c8fb770cc96116dc4cd42ef2b4665c2552
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-10-28 10:56:58 -0500

    more benchs

--- src/main/scala/SafeJsonStringTest.scala
+++ src/main/scala/SafeJsonStringTest.scala
@@ -4,6 +4,20 @@ import org.openjdk.jmh.annotations._
 
 import java.util.concurrent.TimeUnit
 
+/*
+[info] Benchmark                       Mode  Cnt      Score      Error  Units
+[info] SafeJsonStringTest.base_arabic  avgt   50  40829.792 ± 2377.839  ns/op
+[info] SafeJsonStringTest.base_long    avgt   50   4001.831 ±   55.468  ns/op
+[info] SafeJsonStringTest.base_medium  avgt   50    834.071 ±   21.048  ns/op
+[info] SafeJsonStringTest.base_short   avgt   50    305.355 ±    5.344  ns/op
+[info] SafeJsonStringTest.opt1_long    avgt   50    900.917 ±   11.059  ns/op
+[info] SafeJsonStringTest.opt1_medium  avgt   50    168.699 ±    3.016  ns/op
+[info] SafeJsonStringTest.opt1_short   avgt   50     72.518 ±    2.381  ns/op
+[info] SafeJsonStringTest.opt2_arabic  avgt   50  23291.864 ±  834.580  ns/op
+[info] SafeJsonStringTest.opt3_arabic  avgt   50  47596.373 ± 2232.702  ns/op
+[info] SafeJsonStringTest.opt4_arabic  avgt   50  3515.007  ±  128.903  ns/op
+*/
+
 @State(Scope.Thread)
 @BenchmarkMode(Array(Mode.AverageTime))
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
@@ -81,41 +95,65 @@ class SafeJsonStringTest {
       }
       sb.toString
     }
+
+    def opt3(s: String): String = {
+      val sb = new StringBuilder(s.size)
+      var i = 0
+      while (i < s.length) {
+        val c = s charAt i
+        if (isSafe(c)) sb.append(c)
+        else sb.append(f"\\u${c.toInt}%04X")
+        i += 1
+      }
+      sb.toString
+    }
+
+    def opt4(s: String): String = {
+      val sb = new StringBuilder(s.size)
+      var i = 0
+      while (i < s.length) {
+        val c = s charAt i
+        if (isSafe(c)) sb.append(c)
+        else {
+          if (c <= '\u000f') sb.append("\\u000")
+          if (c <= '\u00ff') sb.append("\\u00")
+          if (c <= '\u0fff') sb.append("\\u0")
+          else sb.append("\\u")
+          sb.append(c.toInt.toHexString.toUpperCase)
+        }
+        i += 1
+      }
+      sb.toString
+    }
   }
 
-  // @Benchmark
-  // def base_short = impls.base(strings.short)
-
-  // @Benchmark
-  // def base_medium = impls.base(strings.medium)
-
-  // @Benchmark
-  // def base_long = impls.base(strings.long)
-
-  // @Benchmark
-  // def base_arabic = impls.base(strings.arabic)
-
-  // @Benchmark
-  // def opt1_short = impls.opt1(strings.short)
-
-  // @Benchmark
-  // def opt1_medium = impls.opt1(strings.medium)
-
-  // @Benchmark
-  // def opt1_long = impls.opt1(strings.long)
-
-  // @Benchmark
-  // def opt1_arabic = impls.opt1(strings.arabic)
+  @Benchmark
+  def base_short = impls.base(strings.short)
 
   @Benchmark
-  def opt2_short = impls.opt2(strings.short)
+  def base_medium = impls.base(strings.medium)
 
   @Benchmark
-  def opt2_medium = impls.opt2(strings.medium)
+  def base_long = impls.base(strings.long)
 
   @Benchmark
-  def opt2_long = impls.opt2(strings.long)
+  def base_arabic = impls.base(strings.arabic)
+
+  @Benchmark
+  def opt1_short = impls.opt1(strings.short)
+
+  @Benchmark
+  def opt1_medium = impls.opt1(strings.medium)
+
+  @Benchmark
+  def opt1_long = impls.opt1(strings.long)
 
   @Benchmark
   def opt2_arabic = impls.opt2(strings.arabic)
+
+  @Benchmark
+  def opt3_arabic = impls.opt3(strings.arabic)
+
+  @Benchmark
+  def opt4_arabic = impls.opt4(strings.arabic)
 }
