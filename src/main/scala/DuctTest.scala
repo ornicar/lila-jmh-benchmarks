fe6d3b707f0a83ccfdbee52ede5259a545eba878
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-08-21 21:07:45 +0200

    add DuctTest

---
+++ src/main/scala/DuctTest.scala
@@ -0,0 +1,87 @@
+package org.openjdk.jmh.samples
+
+import org.openjdk.jmh.annotations._
+
+import java.util.concurrent.TimeUnit
+
+import scala.collection.immutable.Queue
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.concurrent.stm._
+
+@State(Scope.Thread)
+@BenchmarkMode(Array(Mode.AverageTime))
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+class DuctTest extends Zero.Syntax {
+
+  type ReceiveAsync = PartialFunction[Any, Future[Any]]
+
+  trait DuctSTM {
+
+    // implement async behaviour here
+    protected val process: ReceiveAsync
+
+    case class State(busy: Boolean, queue: Queue[Any]) {
+      def enqueue(msg: Any) = copy(queue = queue enqueue msg)
+    }
+
+    def !(msg: Any): Unit = atomic { implicit txn =>
+      stateRef.transform { state =>
+        if (state.busy) state enqueue msg
+        else {
+          Txn.afterCommit { _ => run(msg) }
+          state.copy(busy = true)
+        }
+      }
+    }
+
+    private[this] val stateRef = Ref(State(false, Queue.empty))
+
+    private[this] def run(msg: Any): Unit =
+      process.applyOrElse(msg, fallback) onComplete { _ => postRun }
+
+    private[this] def postRun = atomic { implicit txn =>
+      stateRef.transform { state =>
+        state.queue.dequeueOption match {
+          case None => state.copy(busy = false)
+          case Some((msg, newQueue)) =>
+            Txn.afterCommit { _ => run(msg) }
+            State(true, newQueue)
+        }
+      }
+    }
+  }
+
+  trait DuctSynchronized {
+
+    // implement async behaviour here
+    protected val process: ReceiveAsync
+
+    def !(msg: Any): Unit = this.synchronized {
+      current = current flatMap {_ => run(msg) }
+    }
+
+    private[this] var current: Future[Any] = Future.successful(())
+
+    private[this] def run(msg: Any): Future[Any] =
+      process.applyOrElse(msg, fallback).recover { case _ => () }
+  }
+
+  val fallback = { msg: Any =>
+    Future(())
+  }
+
+  val processTrue: ReceiveAsync = {
+    case true => Future.successful(true)
+  }
+
+  val ductSTM = new DuctSTM { val process = processTrue }
+  val ductSynchronized = new DuctSynchronized { val process = processTrue }
+
+  @Benchmark
+  def stm = ductSTM ! true
+
+  @Benchmark
+  def sync = ductSynchronized ! true
+}
